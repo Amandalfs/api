1f6ff168d6dd0435849fb1bfc54844563bfdd613
Amandalfs <caduc57@gmail.com>
2023-05-02 18:04:34 -0300

    feat
    rota de atualizar o nome email do usuario

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -25,14 +25,32 @@ class UsersController {
         const { name, email } = req.body;
         const { id } = req.params;
 
-        const database = sqliteConection();
+        const database = await sqliteConection();
         const user = await database.get("SELECT * FROM users WHERE id = (?)", [id]);
 
         if(!user){
             throw new appError("Usuario nao encontrado");
         }
 
-        const userVerificarAtualizacaoEmail = await database.get("SELECT * FROM users WHERE email = (?)")
+        const userVerificarAtualizacaoEmail = await database.get("SELECT * FROM users WHERE email = (?)", [email]);
+
+        if(userVerificarAtualizacaoEmail && userVerificarAtualizacaoEmail.id !== user.id){
+            throw new  appError("Nao pode mudar email, uma conta com esse email ja existe")
+        }
+
+        user.name = name;
+        user.email = email;
+
+        await database.run(
+        `UPDATE users SET
+        name = ?,
+        email = ?,
+        updated_at = ?
+        WHERE id = ?`,
+        [user.name, user.email, new Date(), id]
+        );
+
+        res.status(200).json()
     }
 }
 

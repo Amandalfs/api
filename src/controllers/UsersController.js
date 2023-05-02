b272cfe2d91ff9e54b82eb1c075f9728d53aa2e7
Amandalfs <caduc57@gmail.com>
2023-05-02 17:45:34 -0300

    feat
    criptografia de senha

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,4 +1,5 @@
 const AppError = require('../utils/AppError');
+const { hash } = require('bcrypt');
 
 const sqliteConection = require('../database/sqlite');
 const appError = require('../utils/AppError');
@@ -14,8 +15,24 @@ class UsersController {
             throw new appError("Este E-mail ja esta em uso")
         }
 
-        await database.run("INSERT INTO users (name, email, password) VALUES (?, ?, ?)", [name, email, password])
+        const hashedPassword = await hash(password, 8);
+
+        await database.run("INSERT INTO users (name, email, password) VALUES (?, ?, ?)", [name, email, hashedPassword])
         return res.status(201).json();
+    }
+
+    async update(req, res){
+        const { name, email } = req.body;
+        const { id } = req.params;
+
+        const database = sqliteConection();
+        const user = await database.get("SELECT * FROM users WHERE id = (?)", [id]);
+
+        if(!user){
+            throw new appError("Usuario nao encontrado");
+        }
+
+        const userVerificarAtualizacaoEmail = await database.get("SELECT * FROM users WHERE email = (?)")
     }
 }
 

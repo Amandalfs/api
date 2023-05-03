6900136ee080a570709adff55d7f01c58e64bbf9
Amandalfs <caduc57@gmail.com>
2023-05-03 08:44:46 -0300

    feat
    listando todas as notas

--- src/controllers/NotesController.js
+++ src/controllers/NotesController.js
@@ -56,13 +56,42 @@ class NotesController{
     }
 
     async index(req, res){
-        const { user_id } = req.query;
+        const { user_id, title, tags } = req.query;
 
-        const notes = await db('notes')
-        .where({ user_id })
-        .orderBy('title');
+        let notes;
 
-        return res.json({notes});
+        if(tags){
+            const filterTags = tags.split(',').map(tag => tag.trim());
+
+            notes = await db('tags')
+            .select([
+                "notes.id",
+                "notes.title",
+                "notes.user_id"
+            ])
+            .where("notes.user_id", user_id)
+            .whereLike("notes.title", `%${title}%`)
+            .whereIn("name", filterTags)
+            .innerJoin("notes", "notes.id", "tags.note_id")
+            .orderBy('title');
+        }else {
+            notes = await db('notes')
+            .where({ user_id })
+            .whereLike("title", `%${title}%`)
+            .orderBy('title');
+            
+        }
+        const userTags = await db('tags').where({ user_id });
+        const notesWithTags = await notes.map(note=>{
+            const noteTags = userTags.filter(userTag => userTag.note_id === note.id)
+
+            return {
+                ...note, 
+                tags: noteTags
+            }
+        })
+
+        return res.json({notesWithTags});
     }
 }
 

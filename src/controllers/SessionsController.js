05d8997f7e5fb38aee02524f0241ad824ab6e1cf
Amandalfs <caduc57@gmail.com>
2023-05-04 13:16:25 -0300

    feat
    envio do token de autenticacao

--- src/controllers/SessionsController.js
+++ src/controllers/SessionsController.js
@@ -1,8 +1,31 @@
+const { compare } = require('bcrypt');
+const db = require('../database/knex');
+const AppError = require('../utils/AppError');
+const AuthConfig = require("../config/auth");
+const { sign } = require('jsonwebtoken');
+
 class SessionsController {
     async create(req, res){
         const { email, password } = req.body;
+
+        const user = await db('users').where({email}).first();
+        const ComparedPassword = await compare(password, user.password);
         
-        return res.json({email, password});
+        if(!user){  
+            throw new AppError("mail e/ou senha Incorreta!");
+        }
+
+        if(!ComparedPassword){
+            throw new AppError("Email e/ou senha Incorreta!");
+        }
+
+        const { secret, expiresIn} = AuthConfig.jwt;
+        const token = sign({}, secret, {
+            subject: String(user.id),
+            expiresIn
+        })
+
+        return res.json({user,  token});
     }
 }
 
